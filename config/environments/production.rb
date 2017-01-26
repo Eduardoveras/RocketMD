efb160053db6f6ac4868d69c63628980c198518b
Eduardo Veras <eduardo.storm@gmail.com>
2017-01-26 11:39:08 -0400

    Setting up email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,10 +65,11 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  #config.action_mailer.default_url_options = { host: 'http://herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'rocketmd.herokuapp.com' }
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => 'utf-8'
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
+
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
@@ -78,6 +79,7 @@ Rails.application.configure do
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
+  config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
