751e21d44399177a47d76da3acf9bf5e9a7a6ae7
Eduardo Veras <eduardo.storm@gmail.com>
2017-01-27 22:09:10 -0400

    Fix error after user confirms account

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -1,6 +1,6 @@
 class ConfirmationsController < Devise::ConfirmationsController
   private
   def after_confirmation_path_for(resource_name, resource)
-    #After the account is confirme, it should redirec to this
+    root_path
   end
 end
