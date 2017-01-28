db42597309dde0a84fa80a69b22e051fe4b2d8c5
Eduardo Veras <eduardo.storm@gmail.com>
2017-01-27 20:20:15 -0400

    Assing the correct homepage for when you are logged in or not

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,7 @@
 class PatientsController < ApplicationController
   before_action :set_patient, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
+
 
   # GET /patients
   # GET /patients.json
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,19 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'plainpage#index'
+   #root 'plainpage#index'
+
+
+  devise_scope :user do
+    authenticated :user do
+      root 'plainpage#index', as: :root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
