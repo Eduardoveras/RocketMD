2f6a71a21142c6c632418d7ea8f450e10a60429e
Eduardo Veras <eduardo.storm@gmail.com>
2017-01-29 20:09:11 -0400

    add date validation, patient profile, styling,etc

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,5 +1,18 @@
 class Patient < ApplicationRecord
   validates_presence_of :first_name, :last_name, :identification, :date_of_birth
+  validate :not_future_date
+
   belongs_to :user
+
+  def name
+    first_name.to_s + " "+ last_name.to_s.capitalize
+  end
+
+
+  def not_future_date
+    unless self.date_of_birth.past?
+      errors.add(:date, 'not in past')
+    end
+  end
 
 end
