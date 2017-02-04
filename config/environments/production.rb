ac505179c3d647f4fc4daadd7be37474ab95c307
Eduardo Veras <eduardo.storm@gmail.com>
2017-02-04 10:48:04 -0400

    another try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   #config.public_file_server.enabled = true = ENV['RAILS_SERVE_STATIC_FILES'].present?
-  config.serve_static_files = true
+  config.public_file_server.enable = true
   config.assets.precompile =  ['*.js', '*.css', '*.css.erb']
   Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
 
